b2716a2c98eec1c7679e5959759594be5f45c073
102932079 <61527439+102932079@users.noreply.github.com>
2020-11-06 17:48:44 +1100

    1.1 database

---
+++ RockPaperScissors_db/Release 1.1/Relese1.1 DDL.sql
@@ -0,0 +1,54 @@
+-- see ERD as picture
+
+-- Relational Schema
+/*
+PLAYER ( Username )
+PRIMARY KEY ( Username )
+
+SELECTION ( Choice )
+PRIMARY KEY ( Choice )
+
+TURN ( Username, DataTimePlayed, PlayerChoice, AIChoice )
+PRIMARY KEY ( Username, DataTimePlayed )
+FOREIGN KEY ( Username ) REFERENCES PLAYER
+FOREIGN KEY ( PlayerChoice ) REFERENCES SELECTION ( Choice )
+FOREIGN KEY ( AIChoice ) REFERENCES SELECTION ( Choice )
+*/
+
+-- DDL Data Definition Language
+
+USE test
+
+GO
+
+IF OBJECT_ID ( 'PLAYER' ) IS NOT NULL
+DROP TABLE PLAYER;
+
+IF OBJECT_ID ( 'SELECTION' ) IS NOT NULL
+DROP TABLE SELECTION;
+
+IF OBJECT_ID ( 'TURN' ) IS NOT NULL
+DROP TABLE TURN;
+
+GO
+
+CREATE TABLE PLAYER (
+    Username    NVARCHAR ( 50 ) NOT NULL
+,   PRIMARY KEY ( Username )
+);
+
+CREATE TABLE SELECTION (
+    Choice  NVARCHAR ( 8 ) NOT NULL
+,   PRIMARY KEY ( Choice )
+);
+
+CREATE TABLE TURN (
+    Username    NVARCHAR ( 50 ) NOT NULL
+,   DateTimePlayed  DateTime NOT NULL
+,   PlayerChoice    NVARCHAR ( 8 ) NOT NULL CHECK( PlayerChoice IN ( 'rock', 'paper', 'scissors' ) )
+,   AIChoice     NVARCHAR ( 8 ) NOT NULL CHECK( AIChoice IN ( 'rock', 'paper', 'scissors' ) )
+,   PRIMARY KEY ( DateTimePlayed, Username )
+,   FOREIGN KEY ( Username ) REFERENCES PLAYER
+,   FOREIGN KEY ( PlayerChoice ) REFERENCES SELECTION ( Choice )
+,   FOREIGN KEY ( AIChoice ) REFERENCES SELECTION ( Choice )
+);
