263e1f6bb096beaa93c7960986025413528444c5
102932079 <61527439+102932079@users.noreply.github.com>
2020-11-06 20:34:39 +1100

    fix a typo and a syntax error

--- RockPaperScissors_WebApi/ApiApplication/Models/LeaderBoardVoteResponseModel.cs
+++ RockPaperScissors_WebApi/ApiApplication/Models/LeaderBoardVoteResponseModel.cs
@@ -7,15 +7,15 @@ namespace ApiApplication.Controllers.Models
         
 
 
-        //prop
-        public LeaderBoardVoteResponseModel(string username, int wins, int turnsPlayed, double winRatio)
-        {
-            this.Username = username;
-            this.Wins = wins;
-            this.TurnsPlayed = turnsPlayed;
-            this.WinRatio = winRatio;
+        //  system generated
+        // public LeaderBoardVoteResponseModel(string username, int wins, int turnsPlayed, double winRatio)
+        // {
+        //     this.Username = username;
+        //     this.Wins = wins;
+        //     this.TurnsPlayed = turnsPlayed;
+        //     this.WinRatio = winRatio;
 
-        }
+        // }//prop
         public string Username { get; set; }
         public int Wins { get; set; }
         public int TurnsPlayed { get; set; }
@@ -33,7 +33,7 @@ namespace ApiApplication.Controllers.Models
         //method
         public void CalculateWinRatio()
         {
-            double ratio = (this.Wins / this.TurnsPlayed) * 100 //percentage 'ratio + %' //system not allow to use 100
+            double ratio = (this.Wins / this.TurnsPlayed) * 100; //percentage 'ratio + %'
             ratio = System.Math.Round(ratio);
 
             this.WinRatio = ratio;
