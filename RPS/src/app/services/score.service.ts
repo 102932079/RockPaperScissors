a6d37693c68a330898375d6fd53071dfe91eda28
102932079 <61527439+102932079@users.noreply.github.com>
2020-11-06 21:21:29 +1100

    create and define scores service need to create new router

--- RPS/src/app/services/score.service.ts
+++ RPS/src/app/services/score.service.ts
@@ -1,10 +1,40 @@
 import { Injectable } from '@angular/core';
-import { }
+import { HttpClient } from '@angular/common/http';
+import { LeaderBoardVoteResponseModel } from '../models/LeaderBoardVoteResponseModel';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ScoreService {
 
-  constructor() { }
+  //create the array for score
+  scores: LeaderBoardVoteResponseModel[] = [];
+  private httpClient: HttpClient;
+
+  constructor(client: HttpClient) { 
+    this.httpClient = client;
+  }
+
+  getLeaderBoard(){
+    this.httpClient.get<LeaderBoardVoteResponseModel[]>("http://localhost:5500/api/vote/leaderboard").subscribe((response) => 
+    {
+      this.scores = response;
+      console.log(response);
+      this.router.navigateByUrl("/Leaderboard");
+    }, (error) => {      
+      if(error.status == 401){        
+        alert("Unauthorized Error")
+      }
+      if(error.status == 405){        
+        alert("Method Not Allowed")
+      }
+      if(error.status == 404){        
+        alert("page not found")
+      }
+      if(error.status == 500){        
+        alert("Internal Server Error")
+      }      
+    });    
+  }
 }
