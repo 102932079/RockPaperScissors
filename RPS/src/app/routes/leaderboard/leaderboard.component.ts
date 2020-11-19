9d8a0fdac6ec4eb2a4a4238caca7d00cdc58eeae
102932079 <61527439+102932079@users.noreply.github.com>
2020-11-19 17:19:03 +1100

    Fix some css for result page

--- RPS/src/app/routes/leaderboard/leaderboard.component.ts
+++ RPS/src/app/routes/leaderboard/leaderboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ScoreService } from "../../services/score.service";
 import { Router } from '@angular/router';
+import { GameService } from 'src/app/services/game.service';
 
 @Component({
   selector: 'app-leaderboard',
@@ -9,9 +10,14 @@ import { Router } from '@angular/router';
 })
 export class LeaderboardComponent implements OnInit {
   //recall service 
-  constructor(public scoreService: ScoreService, private router: Router) { } //!need import router here and top
+  constructor(public gameService: GameService, public scoreService: ScoreService, private router: Router) { } //!need import router here and top
 
-  ngOnInit(): void {
+  ngOnInit(): void { //` add a back to play compo page in case get stuck and import the game service again on top and ctor
+    
+    //if ( this.gameService.playerSelection == null )
+    //{
+     // this.router.navigateByUrl("/play");
+    //}
     this.scoreService.getLeaderBoard();
   }
 
